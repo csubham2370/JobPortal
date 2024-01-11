92c28d1f872fe801a6ea595990d908e7a306488f
csubham2370 <144363196+csubham2370@users.noreply.github.com>
2024-01-11 20:04:26 +0530

    5th

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,8 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
+python3.11.5 pip install --upgrade pip
+python3.11.5 -m pip install -r requirements.txt
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
+python3.11.5 manage.py makemigrations --noinput
+python3.11.5 manage.py migrate --noinput
+python3.11.5 manage.py collectstatic --noinput --clear
 echo " BUILD END"
