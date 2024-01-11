e03a70e49024a4cd2350d6d865ecbdca479d43c9
csubham2370 <144363196+csubham2370@users.noreply.github.com>
2024-01-11 20:36:57 +0530

    9th

--- JobPortal/urls.py
+++ JobPortal/urls.py
@@ -8,3 +8,6 @@ urlpatterns = [
     path('', include('jobs.urls')),
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
